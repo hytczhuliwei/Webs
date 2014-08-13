56f6709e26db790bc5344c3d3f2bb8f022f8df23
zihua <543367517@qq.com>
2014-08-14 00:14:04 +0800

    acfun,导航栏，新番目录
    导航栏完成，新番目录基本完成

--- Acfun/js/indes.js
+++ Acfun/js/indes.js
@@ -61,10 +61,10 @@ $(function(){
 		}else if(curShowNav=="film"){
 			$(this).find(".largeNav").css({"color":"#E04270"});	
 		}
-		$("#sub-guideMengban").stop(true,false).delay(100).slideDown();
-		$(this).find(".channel").stop(true,false).delay(200).show(100);	
+		$("#sub-guideMengban").stop(true,false).slideDown();
+		$(this).find(".channel").stop(true,false).show();	
 	},function(){
-		$(this).find(".channel").stop(true,false).delay(200).hide();
+		$(this).find(".channel").stop(true,false).hide();
 		$("#sub-guideMengban").stop(true,false).slideUp(300);
 		$(this).find(".largeNav").css({"color":""});	
 	});
@@ -72,8 +72,7 @@ $(function(){
 
 
 
-
-
+	//tab切换
 	$(".tab").click(function(){
 		$(this).parent().find(".tab").attr("class","tab");
 		$(this).attr("class","tab active")
@@ -85,13 +84,38 @@ $(function(){
 	//显示视频全部标题和时间
 	$(".videoBox").hover(function(){
 		$(this).find(".time").animate({"opacity":"1"},500);
-		$(this).find(".info_hover").stop(true,true).fadeOut(50);
-		$(this).find(".videoTitle").animate({"height":"45px"},500);
+		$(this).find(".info_hover").stop(true,true).slideUp(50);
+		$(this).find(".videoTitle").animate({"height":"45px"},400);
 	},function(){
-		$(this).find(".time").animate({"opacity":"0"},500);
-		$(this).find(".info_hover").stop(true,true).fadeIn(50);
-		$(this).find(".videoTitle").animate({"height":"24px"},500);
+		$(this).find(".time").animate({"opacity":"0"},100);
+		$(this).find(".info_hover").stop(true,true).delay(400).slideDown(300);
+		$(this).find(".videoTitle").animate({"height":"16px"},300);
 	});
+
+
+	///每周新番
+	$(".weekStyle").click(function(){
+		$(".xfUpdateTool").hide();
+		$(".xfUpdate").animate({"height":"28px"},500);
+		$(".xfUpdateRight").animate({"height":"28px"},500);
+		$(".weekStyle").css({"background-position":"-232px -367px"});
+
+
+		$(this).parent().parent().find(".xfUpdateRight").animate({"height":"196px"},500);
+		$(this).parent().parent().animate({"height":"196px"},500);
+		$(this).css({"background-position":"-188px -367px"});
+		$(this).parent().parent().find(".xfUpdateTool").show();
+	});
+	$(".btn-scroll").click(function(){
+		var aarray=$(this).parent().parent().find(".xfUpdateRight").children("a");
+		var length=(aarray.length-7)*24;
+		alert(length);
+		$(this).parent().parent().find(".quanbuxinfan").animate({"top":length+"px"},500);
+	});
+
+
+
+
 	//在追剧中显示视频蒙版
 	$(".zhuijuPicArea").hover(function(){
 		$(this).find(".icon").animate({"opacity":"1"},100);
@@ -168,11 +192,11 @@ function lightboxShow(){
 		$(this).animate({"top":picNum*64+1},picNum*10+80,function(){
 		$(".element-animate").animate({"top":"-1px"},80,function(){
 
-		}).animate({"top":"-9px"},90,function(){
+		}).animate({"top":"-10px"},100,function(){
 
-		}).animate({"top":"-3px"},80,function(){
+		}).animate({"top":"-1px"},100,function(){
 
-		}).animate({"top":"-9px"},70,function(){
+		}).animate({"top":"-9px"},100,function(){
 
 		}).animate({"top":"-6px"},50,function(){
 
