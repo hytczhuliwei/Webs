c617197a425bf9cd18af533ebbe79ab89bcaa2e5
zihua <543367517@qq.com>
2014-08-15 12:08:42 +0800

    webQQ-FirstDay
    do friend list and tab change

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -1,3 +1,17 @@
 $(function(){
-	
+	$(".navBtn").click(function(){
+		var btnName=$(this).attr("id");
+		var curPanel=$(".navBtnSelected").attr("id");
+		$(".panel-"+curPanel).hide();
+		$(".navBtnSelected").find(".icon").css({
+			"background-image":""
+		});
+		$(".navBtnSelected").removeClass("navBtnSelected");
+
+		$(this).addClass("navBtnSelected");
+		$(".panel-"+btnName).show();
+		$(this).find(".icon").css({
+			"background-image":'url("css/image/tab_icon_'+btnName+'_selected.png")'
+		});
+	});
 });
