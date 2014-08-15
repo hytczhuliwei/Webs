c427b6398c15ebfe720526a458116a230c82a746
zihua <543367517@qq.com>
2014-08-15 23:32:30 +0800

    webQQ's surface
    webQQ's surface  has been done!

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+	//会话，联系人，控件，设置之间切换
 	$(".navBtn").click(function(){
 		var btnName=$(this).attr("id");
 		var curPanel=$(".navBtnSelected").attr("id");
@@ -14,4 +15,32 @@ $(function(){
 			"background-image":'url("css/image/tab_icon_'+btnName+'_selected.png")'
 		});
 	});
+	//在好友，群，讨论组之间切换
+	$(".memberTitle").click(function(){
+		var clickClass=$(this).attr("class");
+		var clickClass0=clickClass.split(" ")[0];
+		var clickClass1=clickClass.split(" ")[1];
+		var clickClass2=clickClass.split(" ")[2];
+		if(clickClass2==null){
+			var memberTabBodyName=clickClass1.split("member-")[1]
+			$(this).parent().parent().find(".active").removeClass("active");
+			$(this).parent().parent().find(".memberTabBody-"+memberTabBodyName).addClass("active");
+			$(".memberTabSelected").removeClass("memberTabSelected");
+			$(this).addClass("memberTabSelected");
+		}
+		
+	});
+
+	//点开好友分组里的好友
+	$(".friendlistTitle").click(function(){
+		var listState=$(this).parent().attr("class");
+		if(listState!="friendlist"){
+			$(this).parent().removeClass("active");
+		}else{
+			$(this).parent().addClass("active");
+		}
+	});
+
+
+
 });
