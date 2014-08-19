f1319d306209bec8b9948d4d8ab8c098f11f4d26
zihua <543367517@qq.com>
2014-08-19 23:41:44 +0800

    shengda loginweb 1day's night
    wrap frame is done

--- shengda/js/index.js
+++ shengda/js/index.js
@@ -1,5 +1,6 @@
 $(function(){
 	//setTimeout("readyWeb()",500);
+	//收起或拉下手机快速注册窗口
 	$(".slide_top").click(function(){
 		var down_txtDisplay=$(this).find(".down_txt").attr("style");
 		var slide_toggDisplay=$(this).find(".slide_togg").attr("style");
@@ -22,11 +23,72 @@ $(function(){
 	 	$(".mode_aimate_wrap .slide_top").animate({"top":"20px"},800);
 	 	$(".slide_bg").slideUp(800);
 	});
+	//弹出手机下一步窗口
 	$(".setup_btn").click(function(){
 		$(".tran_slide_box1").animate({"left":"0"},500);
 	});
 	$("#btnRightArr").click(function(){
 		$(".tran_slide_box1").animate({"left":"466px"},500);
+	});
+
+	//切换手机注册，邮箱注册，个性注册三种方式
+	$(".mod_regist_nav li").click(function(){
+		if($(this).attr("class")=="cur"){
+			return false;
+		}
+		var selectedTab=$(this).attr("name");
+		if(selectedTab=="registPhone"){
+			$(".verifyCode").hide();
+		}else{
+			$(".verifyCode").show();
+		}
+		$("#mod_regist_form").attr("class","mod_regist_form "+selectedTab);
+		$(this).parent().find(".cur").removeClass("cur");
+		$(this).addClass("cur");
+	});
+	//手机号栏
+	$("#mobile").click(function(){
+		$(this).parent().parent().find(".tipsInfo").show();
+		$(this).parent().parent().find(".tipsError").hide();
+		return false;
+	});
+	$(document).click(function(){
+		$(".tipsInfo").hide();
+		$(".tipsError").show();
+	});
+	//切换到外国号码输入区
+	$("#js_get_flags").click(function(){
+		if($(this).html()!="点击这里"){
+			$(this).prev().html("中国大陆号码？");
+			$(this).html("点击这里");
+			$(this).parent().parent().parent().find(".otherMb").show();
+			$(this).parent().parent().parent().addClass("otherMbInput");
+			return false;
+		}else{
+			$(this).prev().html("11位数字，");
+			$(this).html("非中国大陆号码点击这里");
+			$(this).parent().parent().parent().find(".otherMb").hide();
+			$(this).parent().parent().parent().removeClass("otherMbInput");
+			return false;
+		}
+	});
+
+	// 切换不同国家的手机区号
+	$(".flag_wrap,#flag_list li").click(function(){
+		var phonequhao=$(this).find(".flag_name").html().split("+")[1];
+		var guoqiClass=$(this).find("span").eq(0).attr("class");
+		$("#country_flag").find("span").eq(0).attr("class",guoqiClass);
+		$("#country_code").html("+"+phonequhao);
+	});
+	$(".selcountry_inner").click(function(){
+		$("#flag_list").show();
+		return false;
+	});
+	$(".country_search").click(function(){
+		return false;
+	});
+	$(document).click(function(){
+		$("#flag_list").hide();
 	});
 });
 
