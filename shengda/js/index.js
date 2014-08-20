6d831f638bbff3877aaeb3b35c1a9606a0535ce6
zihua <543367517@qq.com>
2014-08-20 12:05:08 +0800

    shengda loginweb 2day'noon
    do some confirmation of text input

--- shengda/js/index.js
+++ shengda/js/index.js
@@ -47,30 +47,63 @@ $(function(){
 		$(this).addClass("cur");
 	});
 	//手机号栏
-	$("#mobile").click(function(){
+	$("#mobile").focus(function(){
+		if($("#mobile").parent().parent().attr("class")=="input_wrap otherMbInput"){
+			$("#js_get_flags").prev().html("中国大陆号码？");
+			$("#js_get_flags").html("点击这里");
+		}else{
+			$("#js_get_flags").prev().html("11位数字，");
+			$("#js_get_flags").html("非中国大陆号码点击这里");
+		}
+
 		$(this).parent().parent().find(".tipsInfo").show();
 		$(this).parent().parent().find(".tipsError").hide();
-		return false;
 	});
-	$(document).click(function(){
-		$(".tipsInfo").hide();
-		$(".tipsError").show();
+	//$(document).click(function(){
+		$("#mobile").blur(function(){
+	    reg=/^1[3|4|5|8][0-9]\d{4,8}$/i;//验证手机正则
+	    var html="";
+		if($("#mobile").val()!="" && !reg.test($("#mobile").val())){
+			if($("#mobile").parent().parent().attr("class")=="input_wrap otherMbInput"){
+				html+='手机号码格式错误';
+			}else{
+				html+='<span class="phoneerrortip">手机号码填写不正确，</span>';
+				html+='<a class="js_get_flags" hidefocus="true">';
+				html+='非中国大陆号码点击这里';
+				html+='</a>';
+			}										
+											
+		}else if($("#mobile").val()==""){
+			html+='手机号码未填写';
+		}else if(reg.test($("#mobile").val())){
+			$("#mobileTip p").hide();
+			$("#mobile").parent().removeClass("inputError");
+			$("#mobile").parent().addClass("inputSuc");
+			return;
+		}
+		$(".js_get_flags").trigger("click");
+		$("#mobileTip .tipsError").html(html);
+		$("#mobileTip .tipsInfo").hide();
+		$("#mobileTip .tipsError").show();
 	});
 	//切换到外国号码输入区
-	$("#js_get_flags").click(function(){
+	$(document).on("click",".js_get_flags",function(){
+		$("#mobileTip .tipsError").hide();
+		$("#mobileTip .tipsInfo").show();
 		if($(this).html()!="点击这里"){
 			$(this).prev().html("中国大陆号码？");
 			$(this).html("点击这里");
 			$(this).parent().parent().parent().find(".otherMb").show();
 			$(this).parent().parent().parent().addClass("otherMbInput");
-			return false;
+			
 		}else{
 			$(this).prev().html("11位数字，");
 			$(this).html("非中国大陆号码点击这里");
 			$(this).parent().parent().parent().find(".otherMb").hide();
 			$(this).parent().parent().parent().removeClass("otherMbInput");
-			return false;
+
 		}
+		$("#mobile").trigger("focus");
 	});
 
 	// 切换不同国家的手机区号
